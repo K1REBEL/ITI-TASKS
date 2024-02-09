d6de12308551180973d0f4eb1de18e2614925622
K1REBEL <kibraheem47@yahoo.com>
2024-02-10 01:27:26 +0200

    Small Update

--- Laravel/Project/resources/views/includes/navbar.blade.php
+++ Laravel/Project/resources/views/includes/navbar.blade.php
@@ -6,8 +6,8 @@
         <li class="nav-item dropdown">
             <a href="#" class="nav-link dropdown-toggle">Dropdown</a>
             <ul class="dropdown-menu">
-                <li><a href="127.0.0.1:8000/users/index">List</a></li>
-                <li><a href="127.0.0.1:8000/users/create">New User</a></li>
+                <li><a href="users/">List</a></li>
+                <li><a href="users/create">New User</a></li>
             </ul>
         </li>
     </ul>
