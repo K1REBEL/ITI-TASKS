ee112859781dd5ec364b3da6278de2621bfaa5a5
K1REBEL <kibraheem47@yahoo.com>
2024-03-01 00:36:06 +0200

    Day 1 ReactJS

---
+++ ReactJS/day1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Task1 from './task1'
+import Task2 from './task2'
+
+function App() {
+  return (
+    <div className="App">
+      <Task1/>
+      <hr></hr>
+      <Task2/>
+    </div>
+  );
+}
+
+export default App;
