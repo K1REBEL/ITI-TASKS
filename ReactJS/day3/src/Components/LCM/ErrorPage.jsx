876ff476049143d4cbbe8cfd3182bd16e203fa9f
K1REBEL <kibraheem47@yahoo.com>
2024-03-11 12:39:58 +0200

    ReactJS Day 3

---
+++ ReactJS/day3/src/Components/LCM/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ErrorPage extends Component {
+  render() {
+    return (
+      <>
+        <h2 className="alert alert-danger">Error, sorry....</h2>
+      </>
+    );
+  }
+}
+
+export default ErrorPage;
